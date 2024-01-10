79829196dc0733486353dd9aa3ec3ec5bab16243
dulminiguruge <dkguruge@gmail.com>
2024-01-10 09:45:16 -0600

    Updated

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I am Dulmini!";
+const bio1 = "A Software Developer";
+const bio2 = "Passionate about writing clean code";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
